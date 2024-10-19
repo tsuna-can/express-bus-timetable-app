29eff4269b8eb28d32cbb7d2052458e551a41a56
tsunacan <sin2016iba@gmail.com>
2024-10-19 13:42:09 +0900

    Add proto file and serializer

---
+++ frontend/app/src/main/java/com/tsunacan/expressbustimetableapp/data/DefaultBusStopSerializer.kt
@@ -0,0 +1,21 @@
+package com.tsunacan.expressbustimetableapp.data
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import com.tsunacan.expressbustimetableapp.DefaultBusStop
+import java.io.InputStream
+import java.io.OutputStream
+
+object DefaultBusStopSerializer : Serializer<DefaultBusStop> {
+    override val defaultValue: DefaultBusStop = DefaultBusStop.getDefaultInstance()
+    override suspend fun readFrom(input: InputStream): DefaultBusStop {
+        try {
+            return DefaultBusStop.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: DefaultBusStop, output: OutputStream) = t.writeTo(output)
+}
