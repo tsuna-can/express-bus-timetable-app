4533fe30238a67f32bbeddd2ec7b03622c50a489
tsunacan <sin2016iba@gmail.com>
2025-05-10 15:56:59 +0900

    read env variables of DB connection information

--- backend/infrastructure/db.go
+++ backend/infrastructure/db.go
@@ -1,16 +1,25 @@
 package infrastructure
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"log"
 )
 
 var db *sqlx.DB
 
 func NewDb() (*sqlx.DB, error) {
 	var err error
-	connStr := "postgres://myuser:mypassword@localhost:5432/mydatabase?sslmode=disable"
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("user=%s password=%s host=%s dbname=%s", user, password, host, dbname)
 	db, err = sqlx.Connect("postgres", connStr)
 	if err != nil {
 		log.Printf("Error connecting to database: %v", err)
@@ -19,4 +28,3 @@ func NewDb() (*sqlx.DB, error) {
 
 	return db, nil
 }
-
