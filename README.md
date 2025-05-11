de75006e00f438066d1554de7f3f80826fcbb58b
tsunacan <sin2016iba@gmail.com>
2025-05-11 19:22:01 +0900

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Express Bus Time Table app
+Ths is a simple WearOS app that provides a bus time table for a given route. 
 
+# Features
+
+## App
+The app consists of the following three screens.
+
+| Screen | Description | Screenshot |
+|--------|-------------|------------|
+| Route List Screen | Displays a list of available express bus routes. | ![Route List Screen](images/route_list.png) |
+| Bus Stop List Screen | Displays a list of bus stops associated with the selected route. | ![Bus Stop List Screen](images/bus_stop_list_1.png) |
+| Timetable Screen | Displays the timetable for the selected bus stop. You can also register bus stops to be shown on a tile. | ![Timetable Screen](images/timetable_1.png) | 
+
+## Tile
+
+| Screen | Description | Screenshot |
+|--------|-------------|------------|
+| Tile | The tile displays the upcoming timetable for the registered bus stop. | ![Tile](images/tile.png) | 
+
+# Architecure
+```mermaid
+graph LR
+    MobileApp[Mobile App] -->|Request| API
+    API -->|Response| MobileApp
+
+    subgraph koyeb[Cloud: Koyeb]
+        API[API Server] -->|Query| DB[(Database)]
+        DB -->|Data| API
+    end
+```
+
+# Tech Stack
+- Mobile
+    - Kotlin
+    - Jetpack Compose
+- Backend
+    - Go
+    - Echo
+- Database
+    - PostgreSQL
+- Hosting
+    - [Koyeb](https://www.koyeb.com/)
--- database/init/insert-dummy-data.sql
+++ database/init/insert-dummy-data.sql
@@ -1,17 +1,16 @@
 INSERT INTO ParentRoute (parent_route_id, parent_route_name) VALUES
-    ('parent-route-1', '鹿島号（上り）'),
-    ('parent-route-2', '鹿島号（下り）');
+    ('parent-route-1', '千葉号（上り）'),
+    ('parent-route-2', '千葉号（下り）');
 
 INSERT INTO Stop (stop_id, stop_name) VALUES
     ('stop-1', '東京駅'),
-    ('stop-2', '水郷潮来BT'),
-    ('stop-3', 'アートホテル鹿島セントラル'),
-    ('stop-4', '鹿島神宮駅');
+    ('stop-2', '船橋駅'),
+    ('stop-3', '幕張'),
+    ('stop-4', '千葉駅');
 
 INSERT INTO Route (route_id, parent_route_id, route_name, origin_stop_id, destination_stop_id) VALUES
     ('route-1', 'parent-route-1', '東京駅行き', 'stop-4', 'stop-1'),
-    ('route-2', 'parent-route-2', '鹿島神宮駅行き', 'stop-1', 'stop-4'),
-    ('route-3', 'parent-route-2', 'アートホテル鹿島セントラル行き', 'stop-3', 'stop-1');
+    ('route-2', 'parent-route-2', '千葉駅行き', 'stop-1', 'stop-4');
 
 INSERT INTO Calendar (service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, description) VALUES
     ('weekday-id', TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, 'weekday service'),
@@ -20,14 +19,14 @@ INSERT INTO Calendar (service_id, monday, tuesday, wednesday, thursday, friday, 
 INSERT INTO Trip (trip_id, route_id, service_id) VALUES
     ('trip-1', 'route-1', 'weekday-id'),
     ('trip-2', 'route-1', 'weekday-id'),
-    ('trip-3', 'route-1', 'holiday-id'),
-    ('trip-4', 'route-2', 'weekday-id'),
-    ('trip-5', 'route-2', 'holiday-id'),
-    ('trip-6', 'route-3', 'weekday-id'),
-    ('trip-7', 'route-3', 'holiday-id');
+    ('trip-3', 'route-1', 'weekday-id'),
+    ('trip-4', 'route-1', 'weekday-id');
 
 INSERT INTO StopTime (trip_id, stop_id, arrival_time, departure_time, stop_sequence, pickup_only_flag, drop_off_only_flag) VALUES
-    ('trip-1', 'stop-2', '05:05', '05:05', 1, TRUE, FALSE),
-    ('trip-1', 'stop-1', '06:40', '06:40', 2, FALSE, TRUE),
-    ('trip-2', 'stop-2', '05:25', '05:25', 1, FALSE, TRUE),
-    ('trip-2', 'stop-1', '06:55', '06:55', 1, TRUE, FALSE);
+    ('trip-1', 'stop-4', '19:00', '19:00', 1, TRUE, FALSE),
+    ('trip-1', 'stop-3', '19:10', '19:10', 1, TRUE, FALSE),
+    ('trip-1', 'stop-2', '19:35', '19:35', 1, TRUE, FALSE),
+    ('trip-1', 'stop-1', '20:00', '20:00', 1, TRUE, FALSE),
+    ('trip-2', 'stop-4', '19:15', '19:15', 1, TRUE, FALSE),
+    ('trip-3', 'stop-4', '19:30', '19:30', 1, TRUE, FALSE),
+    ('trip-4', 'stop-4', '19:45', '19:45', 1, TRUE, FALSE);
